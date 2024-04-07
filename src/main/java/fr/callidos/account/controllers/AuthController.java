bc69fa6f02f032ea22fba2d66e42cb7815ec27de
Respowerr <respowerryt@gmail.com>
2024-04-07 23:08:01 +0200

    clean

--- src/main/java/fr/callidos/account/controllers/AuthController.java
+++ src/main/java/fr/callidos/account/controllers/AuthController.java
@@ -25,7 +25,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/account")
