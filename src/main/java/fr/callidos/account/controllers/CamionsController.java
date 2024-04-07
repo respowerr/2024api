39acfd2a474176729f0d3e5137f8d50a4a799692
Respowerr <respowerryt@gmail.com>
2024-04-07 23:12:42 +0200

    Authorize only for ROLE_ADMIN and ROLE_USER for GET

--- src/main/java/fr/callidos/account/controllers/CamionsController.java
+++ src/main/java/fr/callidos/account/controllers/CamionsController.java
@@ -5,6 +5,7 @@ import fr.callidos.account.repository.CamionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
@@ -16,25 +17,26 @@ public class CamionsController {
     @Autowired
     private CamionRepository camionRepository;
 
+    @PreAuthorize("hasRole('ROLE_ADMIN') OR hasRole('ROLE_USER')")
     @GetMapping("/{id}")
     public ResponseEntity<CamionsModel> getCamionById(@PathVariable Long id) {
         CamionsModel camion = camionRepository.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Camion introuvable avec l'ID : " + id));
         return ResponseEntity.ok(camion);
     }
-
+    @PreAuthorize("hasRole('ROLE_ADMIN') OR hasRole('ROLE_USER')")
     @GetMapping
     public ResponseEntity<List<CamionsModel>> getCamions() {
         List<CamionsModel> camions = camionRepository.findAll();
         return ResponseEntity.ok(camions);
     }
-
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping
     public ResponseEntity<String> ajouterCamion(@RequestBody CamionsModel camion) {
         camionRepository.save(camion);
         return ResponseEntity.status(HttpStatus.CREATED).body("Le camion a été ajouté avec succès.");
     }
-
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PutMapping("/{id}")
     public ResponseEntity<String> modifierCamion(@PathVariable Long id, @RequestBody CamionsModel camionDetails) {
         CamionsModel camion = camionRepository.findById(id)
@@ -45,7 +47,7 @@ public class CamionsController {
         camionRepository.save(camion);
         return ResponseEntity.ok("Le camion a été modifié avec succès.");
     }
-
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<String> supprimerCamion(@PathVariable Long id) {
         camionRepository.deleteById(id);
