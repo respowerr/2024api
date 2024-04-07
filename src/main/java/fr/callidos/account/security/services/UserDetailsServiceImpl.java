65e89b32e034448fd562203c1e04fc5414fec89e
Respowerr <respowerryt@gmail.com>
2024-04-07 15:00:38 +0200

    Create user service

--- src/main/java/fr/callidos/account/security/services/UserDetailsServiceImpl.java
+++ src/main/java/fr/callidos/account/security/services/UserDetailsServiceImpl.java
@@ -1,4 +1,26 @@
 package fr.callidos.account.security.services;
 
-public class UserDetailsServiceImpl {
+import fr.callidos.account.models.User;
+import fr.callidos.account.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("The username " + username + " does not exist."));
+
+        return UserDetailsImpl.build(user);
+    }
+
 }
