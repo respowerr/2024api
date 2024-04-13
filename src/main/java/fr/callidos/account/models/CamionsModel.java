ec2601f9d439b5c6abfdbce004cb95c3a1fffd83
Respowerr <respowerryt@gmail.com>
2024-04-14 01:32:49 +0200

    rm setId

--- src/main/java/fr/callidos/account/models/CamionsModel.java
+++ src/main/java/fr/callidos/account/models/CamionsModel.java
@@ -33,10 +33,6 @@ public class CamionsModel {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getPlaqueImmatriculation() {
         return plaqueImmatriculation;
     }
