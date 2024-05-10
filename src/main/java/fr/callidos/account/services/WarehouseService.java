9fd8ad1c9d2579afbad2d84e8dd1af97a2604c0d
Respowerr <respowerryt@gmail.com>
2024-05-10 02:01:56 +0200

    Init Laon & Saint-Quentin warehouses

--- src/main/java/fr/callidos/account/services/WarehouseService.java
+++ src/main/java/fr/callidos/account/services/WarehouseService.java
@@ -2,9 +2,21 @@ package fr.callidos.account.services;
 
 import fr.callidos.account.models.WarehouseModel;
 import fr.callidos.account.repository.WarehouseRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WarehouseService {
-    //TODO : FAIRE LE WAREHOUSE SERVICE POUR INIT LES DEUX ENTREPOTS
+    @Autowired
+    private WarehouseRepository warehouseRepository;
+    @PostConstruct
+    public void init(){
+        if (warehouseRepository.count() == 0){
+            WarehouseModel SQ = new WarehouseModel(null, "Saint-Quentin", 90, 0);
+            WarehouseModel LAON = new WarehouseModel(null, "Laon", 59, 0);
+            warehouseRepository.save(SQ);
+            warehouseRepository.save(LAON);
+        }
+    }
 }
