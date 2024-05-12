852b01e2e2e9e6b696163b6157753bf558ae7200
Respowerr <respowerryt@gmail.com>
2024-05-12 02:55:37 +0200

    @JsonIgnore password

--- src/main/java/fr/callidos/account/models/User.java
+++ src/main/java/fr/callidos/account/models/User.java
@@ -1,5 +1,6 @@
 package fr.callidos.account.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -43,6 +44,7 @@ public class User {
 
     @NotBlank
     @Size(max = 120)
+    @JsonIgnore
     private String password;
 
     @NotBlank
